83c17183fee6ba0d718ce363824a1303a9e812ca
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-10-03 09:26:59 +0200

    Page completed

--- index.php
+++ index.php
@@ -7,7 +7,11 @@
 </head>
 <body>
     <?php
-        
+        $linkBase = "https://xkcd.com/";
+        $numLink = rand(2,5);
+        for ($i = 1; $i <= $numLink; $i++) {
+            echo "<a href='{$linkBase}$i' target='_blank'>Link alla pagina $i</a><br>";
+        }
     ?>
 </body>
 </html>
